5f34211e83b1324bc6f26e67f9d5a1dca33e9279
ciffitsupport <ciffitsupport@ciff.org>
2024-01-17 13:37:13 +0000

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./ciff_impact_porta/",
   plugins: [react()],
   server: {
     fs: {
