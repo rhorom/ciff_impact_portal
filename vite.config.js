a1d09fb92b5af2bd3ec31e056b3d311f89f5c85c
rhorom <rhorom.priyatikanto@brin.go.id>
2024-01-17 16:33:47 +0000

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ciff_impact_portal/",
+  base: "",
   plugins: [react()],
   server: {
     fs: {
       allow: ['..',]
     }
-  },
-  esbuild: {
-    loader: 'jsx',
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
   },
 })
